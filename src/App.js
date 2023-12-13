dbdf7ab83f4465265f4d0ef41a349c7c127c9055
PlamenCode <plamenggg233@gmail.com>
2023-12-13 14:31:47 +0200

    header component and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {} from 'react';
 import './App.css';
+
+import Header from './components/ui/Header';
 
 function App() {
   return (
-    <div className="App">
-        <h1>helo</h1>
+    <div className="container">
+        <Header />
+
     </div>
   );
 }
---
+++ src/components/ui/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import breakingBadLogo from '../../assets/breakingBadLogo.png';
+
+export default function Header() {
+  return (
+    <header className='center'>
+        <img src={breakingBadLogo} alt="No Img" />
+    </header>
+  )
+}
